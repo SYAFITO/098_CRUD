598397948d3a141f946e57b24818fffe52151e7a
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2025-10-20 10:29:59 +0700

    Query untuk memasukkan data ke tabel 'mahasiswa'

--- index.js
+++ index.js
@@ -51,7 +51,19 @@ app.post('/api/users', (req, res) =>{
         return res.status(400).json({ message: 'nama, nim, kelas wajib diisi'});
     }
 
-   
+    db.query(
+        
+        'INSERT INTO mahasiswa (nama, nim ,kelas) VALUES (?, ?, ?)',
+        [nama, nim, kelas],
+        (err, results) => {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({message: 'Database Error'});
+            }
+
+            res.status(201).json({ message: 'User created successfully'});
+        }
+    );
 });
 
 
